41609312dbfc30e444dc9b8f56de209666f4010f
Rodrigo Aires Noleto <64622959+RodrigoAiresNoleto@users.noreply.github.com>
2023-06-04 22:55:29 -0300

    Calculadora

---
+++ Calculadora/src/main/java/com/web/airessistemas/Airessistemas/AiressistemasApplication.java
@@ -0,0 +1,13 @@
+package com.web.airessistemas.Airessistemas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiressistemasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiressistemasApplication.class, args);
+	}
+
+}
---
+++ Calculadora/src/test/java/com/web/airessistemas/Airessistemas/AiressistemasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.airessistemas.Airessistemas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiressistemasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
