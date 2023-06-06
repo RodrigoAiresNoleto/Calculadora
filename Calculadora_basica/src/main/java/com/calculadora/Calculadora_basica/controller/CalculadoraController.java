2336811be998dffd7df7613df9d9cdc384c76c4b
Rodrigo Aires Noleto <64622959+RodrigoAiresNoleto@users.noreply.github.com>
2023-06-05 23:07:53 -0300

    calculadora

--- Calculadora_basica/src/main/java/com/calculadora/Calculadora_basica/controller/CalculadoraController.java
+++ Calculadora_basica/src/main/java/com/calculadora/Calculadora_basica/controller/CalculadoraController.java
@@ -1,5 +1,14 @@
 package com.calculadora.Calculadora_basica.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/calculadora")
 public class CalculadoraController {
+@PostMapping("/")
+
+  Fpublic CalculadoraResponde rs = Object foo = new Object();
     
 }
